7776dc4e32100e456eea51a2e94ae3edd40a9a79
Quan Nguyen <sendquananemail@gmail.com>
2016-09-08 22:06:34 +0700

    create get-permissions route and generate permission test data when bootstrap app

---
+++ api/Application.Api/Features/Security/PermissionsController.cs
@@ -0,0 +1,33 @@
+﻿using App.Common.DI;
+using App.Common.Http;
+using App.Common.Validation;
+using App.Service.Security;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace App.Api.Features.Security
+{
+    [RoutePrefix("api/permissions")]
+    public class PermissionsController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IResponseData<IList<PermissionListItem>> GetPermissions()
+        {
+            IResponseData<IList<PermissionListItem>> response = new ResponseData<IList<PermissionListItem>>();
+            try
+            {
+                IPermissionService permissionService = IoC.Container.Resolve<IPermissionService>();
+                IList<PermissionListItem> items = permissionService.GetPermissions();
+                response.SetData(items);
+            }
+            catch (ValidationException ex)
+            {
+                response.SetStatus(HttpStatusCode.PreconditionFailed);
+                response.SetErrors(ex.Errors);
+            }
+            return response;
+        }
+    }
+}
---
+++ api/Application.Api/Features/Share/Tasks/Data/CreateTestPermissionsTask.cs
@@ -0,0 +1,22 @@
+﻿using App.Common;
+using App.Common.DI;
+using App.Common.Tasks;
+using App.Service.Security;
+using System.Web;
+
+namespace App.Api.Features.Share.Tasks.Data
+{
+    public class CreatePermissionsTask : BaseTask<TaskArgument<HttpApplication>>, IApplicationReadyTask<TaskArgument<HttpApplication>>
+    {
+        public CreatePermissionsTask() : base(ApplicationType.All)
+        {
+
+        }
+
+        public override void Execute(TaskArgument<HttpApplication> context)
+        {
+            IPermissionService permissionService = IoC.Container.Resolve<IPermissionService>();
+            permissionService.Create(new Entity.Security.Permission("p01", "Add New File", "Add a file to folder"));
+        }
+    }
+}
